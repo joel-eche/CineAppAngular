297c743a614cc3a03b08d5bdfc5f44b640e227bb
Joel Eche <joel.eche5@gmail.com>
2016-12-24 12:30:56 -0500

    puntuación

--- scripts/directives/votacion.js
+++ scripts/directives/votacion.js
@@ -1,13 +1,28 @@
 angular.module("misPelisSeriesApp")
-.directive("votacion",[function(){
+.directive("votacion",["$compile",function($compile){
     return {
         restrict:"AE",
         replace:true,
         scope:{
             media:"@",
         },
-        link:function(scope){
+        link:function(scope,elemento){
             var mediaComoNumero=parseFloat(scope.media);
+            var mediaRedondeada=Math.round(mediaComoNumero);
+            
+            var vista="<div>";
+            for (var f=0;f<mediaRedondeada;f++){
+                vista+="<span class=\"glyphicon glyphicon-star\"></span>";
+            }
+            for (var f=0;f<(10-mediaRedondeada);f++){
+                vista+="<span class=\"glyphicon glyphicon-star-empty\"></span>";
+            }
+            vista+="</div>";
+            
+            var nuevoElemento=angular.element(vista);
+            var nuevoElementoCompilado=$compile(nuevoElemento)(scope);
+            
+            elemento.replaceWith(nuevoElementoCompilado);
         }
     };
 }]);
